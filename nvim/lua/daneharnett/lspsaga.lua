9ec9b130b824b3ae173926697a4376b2c853ce81
Dane Harnett <dane@daneharnett.com>
2021-04-28 18:07:58 +1000

    Key mappings for telescope and lspsaga, comment out nerdtree

--- nvim/lua/daneharnett/lspsaga.lua
+++ nvim/lua/daneharnett/lspsaga.lua
@@ -1,36 +1,42 @@
 local saga = require 'lspsaga'
+local utils = require'daneharnett.utils'
+
 saga.init_lsp_saga()
 
 -- lsp provider to find the cursor word definition and reference
-vim.cmd[[nnoremap <silent> gh <cmd>lua require'lspsaga.provider'.lsp_finder()<CR>]]
+utils.key_mapper('n', 'gh', ':lua require"lspsaga.provider".lsp_finder()<CR>')
 
 -- code action
-vim.cmd[[nnoremap <silent><leader>ca <cmd>lua require('lspsaga.codeaction').code_action()<CR>]]
-vim.cmd[[vnoremap <silent><leader>ca <cmd>'<,'>lua require('lspsaga.codeaction').range_code_action()<CR>]]
+utils.key_mapper('n', '<leader>ca', ':lua require"lspsaga.codeaction".code_action()<CR>')
+utils.key_mapper('v', '<leader>ca', ':<C-U>lua require("lspsaga.codeaction").range_code_action()<CR>')
 
 -- show hover doc
-vim.cmd[[nnoremap <silent> K <cmd>lua require('lspsaga.hover').render_hover_doc()<CR>]]
+utils.key_mapper('n', 'K', ':lua require"lspsaga.hover".render_hover_doc()<CR>')
+
 -- scroll down hover doc or scroll in definition preview
-vim.cmd[[nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<CR>]]
+utils.key_mapper('n', '<C-f>', ':lua require"lspsaga.action".smart_scroll_with_saga(1)<CR>')
+
 -- scroll up hover doc
-vim.cmd[[nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<CR>]]
+utils.key_mapper('n', '<C-b>', ':lua require"lspsaga.action".smart_scroll_with_saga(-1)<CR>')
 
 -- show signature help
-vim.cmd[[nnoremap <silent> gs <cmd>lua require('lspsaga.signaturehelp').signature_help()<CR>]]
+utils.key_mapper('n', 'gs', ':lua require"lspsaga.signaturehelp".signature_help()<CR>')
 
 -- rename
-vim.cmd[[nnoremap <silent> gr <cmd>lua require('lspsaga.rename').rename()<CR>]]
+utils.key_mapper('n', 'gr', ':lua require"lspsaga.rename".rename()<CR>')
 
 -- preview definition
-vim.cmd[[nnoremap <silent> gd <cmd>lua require'lspsaga.provider'.preview_definition()<CR>]]
+utils.key_mapper('n', 'gd', ':lua require"lspsaga.provider".preview_definition()<CR>')
 
 -- show diagnostic
-vim.cmd[[nnoremap <silent><leader>cd <cmd>lua require'lspsaga.diagnostic'.show_line_diagnostics()<CR>]]
+utils.key_mapper('n', '<leader>cd', ':lua require"lspsaga.diagnostic".show_line_diagnostics()<CR>')
+
 -- jump to diagnostic
-vim.cmd[[nnoremap <silent> [e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_prev()<CR>]]
-vim.cmd[[nnoremap <silent> ]e <cmd>lua require'lspsaga.diagnostic'.lsp_jump_diagnostic_next()<CR>]]
+utils.key_mapper('n', '[e', ':lua require"lspsaga.diagnostic".lsp_jump_diagnostic_prev()<CR>')
+utils.key_mapper('n', ']e', ':lua require"lspsaga.diagnostic".lsp_jump_diagnostic_next()<CR>')
 
 -- float terminal also you can pass the cli command in open_float_terminal function
-vim.cmd[[nnoremap <silent> <A-d> <cmd>lua require('lspsaga.floaterm').open_float_terminal()<CR>]]
-vim.cmd[[tnoremap <silent> <A-d> <C-\><C-n>:lua require('lspsaga.floaterm').close_float_terminal()<CR>]]
+-- @wtf: some reason ALT-d doesn't work on my mac???
+utils.key_mapper('n', '<A-d>', ':lua require"lspsaga.floaterm".open_float_terminal()<CR>')
+utils.key_mapper('t', '<A-d>', '<C-\\><C-n>:lua require"lspsaga.floaterm".close_float_terminal()<CR>')
 
--- nvim/lua/daneharnett/telescope.lua
+++ nvim/lua/daneharnett/telescope.lua
@@ -2,9 +2,12 @@ local utils = require'daneharnett.utils'
 local load_mappings = function()
 -- telescope
 utils.key_mapper('n', '<C-p>', ':lua require"telescope.builtin".find_files()<CR>')
-utils.key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".live_grep()<CR>')
+utils.key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".file_browser()<CR>')
+utils.key_mapper('n', '<leader>fg', ':lua require"telescope.builtin".git_files()<CR>')
 utils.key_mapper('n', '<leader>fh', ':lua require"telescope.builtin".help_tags()<CR>')
-utils.key_mapper('n', '<leader>fb', ':lua require"telescope.builtin".buffers()<CR>')
+utils.key_mapper('n', '<leader>fs', ':lua require"telescope.builtin".grep_string()<CR>')
+utils.key_mapper('n', '<leader>lb', ':lua require"telescope.builtin".buffers()<CR>')
+utils.key_mapper('n', '<leader>lg', ':lua require"telescope.builtin".live_grep()<CR>')
 end
 load_mappings()
 
