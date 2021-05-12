6aae276c94b50bb9a4eba74b2554a3d8a4a13c4f
Dane Harnett <dane@daneharnett.com>
2021-05-12 16:20:37 +1000

    Include arrow keys and yank config in vscode

--- nvim/lua/daneharnett/init.lua
+++ nvim/lua/daneharnett/init.lua
@@ -1,22 +1,24 @@
 local vim = vim
 if not vim.g.vscode then
--- plugins first
-require'daneharnett.plugins'
+  -- plugins first
+  require'daneharnett.plugins'
 end
+
 -- options second
 require'daneharnett.options'
-if not vim.g.vscode then
--- lsp third
-require'daneharnett.lsp'
-require'daneharnett.lspsaga'
--- the rest
-require'daneharnett.airline'
-require'daneharnett.arrowkeys'
-require'daneharnett.completion'
-require'daneharnett.diagnostics'
--- require'daneharnett.nerdtree'
-require'daneharnett.prettier'
-require'daneharnett.telescope'
-require'daneharnett.treesitter'
 require'daneharnett.yank'
+require'daneharnett.arrowkeys'
+
+if not vim.g.vscode then
+  -- lsp third
+  require'daneharnett.lsp'
+  require'daneharnett.lspsaga'
+  -- the rest
+  require'daneharnett.airline'
+  require'daneharnett.completion'
+  require'daneharnett.diagnostics'
+  -- require'daneharnett.nerdtree'
+  require'daneharnett.prettier'
+  require'daneharnett.telescope'
+  require'daneharnett.treesitter'
 end
