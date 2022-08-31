3e40f31cb8defa0b1893ed5e697bcfb25c4d4441
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2022-08-31 21:37:09 +1000

    feat: only run eslint and prettier when project uses them

--- nvim/.config/nvim/lua/daneharnett/null-ls.lua
+++ nvim/.config/nvim/lua/daneharnett/null-ls.lua
@@ -6,7 +6,16 @@ end
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 
+local is_eslint_project = function(utils)
+    return utils.root_has_file({ ".eslintrc.js" })
+end
+
+local is_prettier_project = function(utils)
+    return utils.root_has_file({ ".prettierrc" })
+end
+
 null_ls.setup({
+    diagnostics_format = "[#{c}] #{m} (#{s})",
     debug = false,
     on_attach = function(_, bufnr)
         local group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
@@ -21,15 +30,27 @@ null_ls.setup({
     end,
     sources = {
         diagnostics.eslint_d.with({
-          timeout = -1,
-        }),
-        formatting.eslint_d.with({
+            -- We only want this source to apply when the project uses eslint.
+            condition = is_eslint_project,
             timeout = -1,
         }),
-        -- formatting.prettier.with({
-        --     prefer_local = "node_modules/.bin",
-        --     timeout = -1,
-        -- }),
+        formatting.eslint_d.with({
+            -- We only want this source to apply when the project uses eslint.
+            condition = is_eslint_project,
+            -- This filter function should prevent eslint_d from applying any
+            -- formats that have been suggested by eslint-plugin-prettier, they
+            -- will be applied by the prettier formatter below.
+            filter = function(diagnostic)
+                return diagnostic.code ~= "prettier/prettier"
+            end,
+            timeout = -1,
+        }),
+        formatting.prettier.with({
+            -- We only want this source to apply when the project uses prettier.
+            condition = is_prettier_project,
+            prefer_local = "node_modules/.bin",
+            timeout = -1,
+        }),
         formatting.stylua,
     },
 })
