f7a35a68746b0ca9c0809873d454026574e7016b
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2022-12-24 14:01:32 +1000

    chore: nvim - refactor packer plugins to a table and loop over it

--- nvim/.config/nvim/lua/daneharnett/plugins.lua
+++ nvim/.config/nvim/lua/daneharnett/plugins.lua
@@ -12,14 +12,13 @@ local util = require("packer.util")
 packer.init({
     package_root = util.join_paths(vim.fn.stdpath("data"), "site", "pack"),
 })
---- startup and add configure plugins
-packer.startup(function()
-    local use = use
 
-    use("wbthomason/packer.nvim")
-
+local plugins = {
+    {
+        "wbthomason/packer.nvim",
+    },
     -- treesitter
-    use({
+    {
         "nvim-treesitter/nvim-treesitter",
         commit = "f0c9a186eab06d7e4dcd78c973dcb60b702ac5d2",
         config = function()
@@ -32,43 +31,39 @@ packer.startup(function()
                 commit = "45c9b2faff4785539a0d0c655440c2465fed985a",
             },
         },
-    })
-    use({
+    },
+    {
         "nvim-treesitter/nvim-treesitter-context",
         commit = "8e88b67d0dc386d6ba1b3d09c206f19a50bc0625",
-    })
-    use({
+    },
+    {
         "nvim-treesitter/playground",
         commit = "90d2b3e1729363f96ce2c23f16129534df893bbf",
-    })
-
+    },
     -- A collection of language packs for Vim.
-    use({
+    {
         "sheerun/vim-polyglot",
         commit = "38282d58387cff48ac203f6912c05e4c8686141b",
-    })
-
+    },
     -- theme
-    use({
+    {
         "catppuccin/nvim",
         as = "catppuccin",
         commit = "047770c18ddf8081873cc6279f640c2dda725bba",
         config = function()
             require("daneharnett.catppuccin").setup()
         end,
-    })
-
+    },
     -- buffer line (top of buffer)
-    use({
+    {
         "akinsho/bufferline.nvim",
         config = function()
             require("daneharnett.bufferline").setup()
         end,
         tag = "v2.8.2",
-    })
-
+    },
     -- status line (bottom of buffer)
-    use({
+    {
         "nvim-lualine/lualine.nvim",
         commit = "3cf45404d4ab5e3b5da283877f57b676cb78d41d",
         config = function()
@@ -80,10 +75,9 @@ packer.startup(function()
                 commit = "2d02a56189e2bde11edd4712fea16f08a6656944",
             },
         },
-    })
-
+    },
     -- lsp
-    use({
+    {
         "neovim/nvim-lspconfig",
         commit = "df17834baeba1b8425c15a31cbf52e6b23115c37",
         config = function()
@@ -107,9 +101,8 @@ packer.startup(function()
                 commit = "44585a0c0085765195e6961c15529ba6c5a2a13b",
             },
         },
-    })
-
-    use({
+    },
+    {
         "tami5/lspsaga.nvim",
         commit = "9ec569a49aa7ff265764081acff9e5da839c13fe",
         config = function()
@@ -121,8 +114,8 @@ packer.startup(function()
                 commit = "df17834baeba1b8425c15a31cbf52e6b23115c37",
             },
         },
-    })
-    use({
+    },
+    {
         "jose-elias-alvarez/null-ls.nvim",
         commit = "de751688c991216f0d17ced7d5076e0c37fa383f",
         config = function()
@@ -134,10 +127,9 @@ packer.startup(function()
                 commit = "df17834baeba1b8425c15a31cbf52e6b23115c37",
             },
         },
-    })
-
+    },
     -- completion
-    use({
+    {
         "hrsh7th/nvim-cmp",
         commit = "8bbaeda725d5db6e4e1be2867a64b43bf547cf06",
         config = function()
@@ -165,17 +157,17 @@ packer.startup(function()
                 commit = "18095520391186d634a0045dacaa346291096566",
             },
         },
-    })
-
-    use({
+    },
+    -- file-tree sidebar explorer
+    {
         "kyazdani42/nvim-tree.lua",
         commit = "011a7816b8ea1b3697687a26804535f24ece70ec",
         config = function()
             require("daneharnett.nvim-tree").setup()
         end,
-    })
-
-    use({
+    },
+    -- harpoon
+    {
         "ThePrimeagen/harpoon",
         commit = "f4aff5bf9b512f5a85fe20eb1dcf4a87e512d971",
         requires = {
@@ -184,10 +176,9 @@ packer.startup(function()
                 commit = "a3dafaa937921a4eb2ae65820c3479ab561e9ba3",
             },
         },
-    })
-
+    },
     -- telescope
-    use({
+    {
         "nvim-telescope/telescope.nvim",
         commit = "cabf991b1d3996fa6f3232327fc649bbdf676496",
         config = function()
@@ -208,9 +199,9 @@ packer.startup(function()
                 run = "make",
             },
         },
-    })
-
-    use({
+    },
+    -- trouble
+    {
         "folke/trouble.nvim",
         commit = "da61737d860ddc12f78e638152834487eabf0ee5",
         config = function()
@@ -222,75 +213,76 @@ packer.startup(function()
                 commit = "2d02a56189e2bde11edd4712fea16f08a6656944",
             },
         },
-    })
-
+    },
     -- editorconfig
-    use({
+    {
         "editorconfig/editorconfig-vim",
         commit = "d354117b72b3b43b75a29b8e816c0f91af10efe9",
-    })
-
+    },
     -- vim-smoothie for smooth scrolling
-    use({
+    {
         "psliwka/vim-smoothie",
         commit = "df1e324e9f3395c630c1c523d0555a01d2eb1b7e",
-    })
-
+    },
     -- comments
-    use({
+    {
         "numToStr/Comment.nvim",
         commit = "80e7746e42fa685077a7941e9022308c7ad6adf8",
         config = function()
             require("daneharnett.comment").setup()
         end,
-    })
-
+    },
     -- autopairs
-    use({
+    {
         "jiangmiao/auto-pairs",
         commit = "39f06b873a8449af8ff6a3eee716d3da14d63a76",
-    })
-
+    },
     -- terminal
-    use({
+    {
         "akinsho/toggleterm.nvim",
         commit = "cd3b4d67112fbc8bee01ea44ba5ad1eea3894714",
         config = function()
             require("daneharnett.toggleterm").setup()
         end,
-    })
-
-    use({
+    },
+    -- run tests
+    {
         "vim-test/vim-test",
         commit = "2da8d59fdc46d1af7b7105a578d1ace3924d1a7b",
         config = function()
             require("daneharnett.tests").setup()
         end,
-    })
-
-    use({
+    },
+    -- show colors visually
+    {
         "norcalli/nvim-colorizer.lua",
         commit = "36c610a9717cc9ec426a07c8e6bf3b3abcb139d6",
         config = function()
             require("daneharnett.colorizer").setup()
         end,
-    })
-
-    use({
+    },
+    -- git
+    {
         "lewis6991/gitsigns.nvim",
         commit = "1e107c91c0c5e3ae72c37df8ffdd50f87fb3ebfa",
         config = function()
             require("daneharnett.gitsigns").setup()
         end,
-    })
-
-    use({
+    },
+    {
         "tpope/vim-fugitive",
         commit = "b411b753f805b969cca856e2ae51fdbab49880df",
-    })
-
-    use({
+    },
+    {
         "APZelos/blamer.nvim",
         commit = "f4eb22a9013642c411725fdda945ae45f8d93181",
-    })
+    },
+}
+
+--- startup and configure plugins
+packer.startup(function()
+    local use = use
+    for _, v in pairs(plugins) do
+        use(v)
+    end
 end)
