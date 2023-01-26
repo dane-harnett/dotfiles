35573da0d946be43794fc6655b4eab2a1a7815a7
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-01-27 09:01:25 +1000

    docs: nvim - update inspired by block to include awesome people

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,8 @@
 --------------------------------------------------------------
 -- config inspired by:
 -- https://bryankegley.me/posts/nvim-getting-started/
+-- https://www.chrisatmachine.com/
+-- https://www.youtube.com/@teej_dv / https://www.twitch.tv/teej_dv
+-- and many others, thank you!
 --------------------------------------------------------------
 require("daneharnett")
