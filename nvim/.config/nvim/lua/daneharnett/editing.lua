8ec982fdf4e1c49d6357328d7285d90e83ccee0f
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-01-28 18:56:40 +1000

    chore: nvim - clean up editing module

--- nvim/.config/nvim/lua/daneharnett/editing.lua
+++ nvim/.config/nvim/lua/daneharnett/editing.lua
@@ -3,25 +3,30 @@ local M = {}
 function M.init()
     local utils = require("daneharnett.utils")
     -- In normal mode...
-    -- Move text up and down
-    utils.keymap("n", "<A-j>", "<Esc>:m .+1<CR>==gi")
-    utils.keymap("n", "<A-k>", "<Esc>:m .-2<CR>==gi")
+    -- Move current line down one line
+    utils.keymap("n", "<A-j>", "<Esc>:m .+1<CR><Esc>")
+    -- Move current line up one line
+    utils.keymap("n", "<A-k>", "<Esc>:m .-2<CR><Esc>")
 
-    -- In visual mode...
-    -- Stay in indent mode
+    -- In visual/visual-line mode...
+    -- Stay in visual/visual-line mode after deindenting
     utils.keymap("v", "<", "<gv")
+    -- Stay in visual/visual-line mode after indenting
     utils.keymap("v", ">", ">gv")
 
-    -- Move text up and down
+    -- Move selected line(s) down one line
     utils.keymap("v", "<A-j>", ":m .+1<CR>==")
+    -- Move selected line(s) up one line
     utils.keymap("v", "<A-k>", ":m .-2<CR>==")
+    -- what is this?
     utils.keymap("v", "p", '"_dP')
 
-    -- In visual block mode...
-    -- Move text up and down
+    -- In visual-block mode...
+    -- Move selected block down one line
     utils.keymap("x", "J", ":move '>+1<CR>gv-gv")
-    utils.keymap("x", "K", ":move '<-2<CR>gv-gv")
     utils.keymap("x", "<A-j>", ":move '>+1<CR>gv-gv")
+    -- Move selected block up one line
+    utils.keymap("x", "K", ":move '<-2<CR>gv-gv")
     utils.keymap("x", "<A-k>", ":move '<-2<CR>gv-gv")
 end
 
