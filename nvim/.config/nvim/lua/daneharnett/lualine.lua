4cbc42f7d9e2124ea8cb9426b6870f70745f7546
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-01-28 19:16:45 +1000

    chore: nvim - add protected call around lualine module

--- nvim/.config/nvim/lua/daneharnett/lualine.lua
+++ nvim/.config/nvim/lua/daneharnett/lualine.lua
@@ -1,7 +1,12 @@
 local M = {}
 
 function M.init()
-    require("lualine").setup({
+    local lualine_status_ok, lualine = pcall(require, "lualine")
+    if not lualine_status_ok then
+        return
+    end
+
+    lualine.setup({
         options = {
             disabled_filetypes = {
                 "NvimTree",
