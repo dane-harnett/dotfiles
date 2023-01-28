7880fb6b50e33f66565498afb3bb3a3ae46d7a7f
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-01-28 19:11:59 +1000

    chore: nvim - refactor treesitter module

--- nvim/.config/nvim/lua/daneharnett/treesitter.lua
+++ nvim/.config/nvim/lua/daneharnett/treesitter.lua
@@ -1,8 +1,22 @@
 local M = {}
 
 function M.init()
-    local treesitter_configs = require("nvim-treesitter.configs")
-    treesitter_configs.setup({
+    local ts_configs_setup_status_ok = M.setup_treesitter_configs()
+    if not ts_configs_setup_status_ok then
+        return
+    end
+
+    M.setup_treesitter_context()
+    M.setup_treesitter_folds()
+end
+
+M.setup_treesitter_configs = function()
+    local ts_configs_status_ok, ts_configs = pcall(require, "nvim-treesitter.configs")
+    if not ts_configs_status_ok then
+        return false
+    end
+
+    ts_configs.setup({
         ensure_installed = {
             "bash",
             "css",
@@ -27,18 +41,29 @@ function M.init()
         },
     })
 
+    return true
+end
+
+M.setup_treesitter_context = function()
     local context_status_ok, tscontext = pcall(require, "treesitter-context")
     if context_status_ok then
         tscontext.setup({})
     end
+end
+
+M.setup_treesitter_folds = function()
+    local ts_parsers_status_ok, ts_parsers = pcall(require, "nvim-treesitter.parsers")
+    if ts_parsers_status_ok then
+        return
+    end
 
     local group = vim.api.nvim_create_augroup("FoldsWithTreesitter", { clear = true })
-    local parsers = require("nvim-treesitter.parsers").available_parsers()
     local additional_file_types = {
         "typescriptreact",
     }
     local all_file_types = {}
 
+    local parsers = ts_parsers.available_parsers()
     for _, filetype in ipairs(parsers) do
         table.insert(all_file_types, filetype)
     end
