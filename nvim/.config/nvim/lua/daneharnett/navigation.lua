07cbb7c602cb71d5f633c8f8796fae11196bdded
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-01-28 12:17:19 +1000

    chore: nvim - remove duplicate keymap for yank setting

--- nvim/.config/nvim/lua/daneharnett/navigation.lua
+++ nvim/.config/nvim/lua/daneharnett/navigation.lua
@@ -1,9 +1,5 @@
 local utils = require("daneharnett.utils")
 local load_mappings = function()
-    -- When in visual mode, after yanking keep the cursor in the last selected
-    -- position.
-    utils.keymap("v", "y", "ygv<Esc>")
-
     -- Normal --
     -- Better window navigation
     utils.keymap("n", "<C-h>", "<C-w>h")
