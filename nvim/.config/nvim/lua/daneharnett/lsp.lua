4794d74ecdad9ea8e8572b31682b215c1b10159b
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2023-02-04 19:25:07 +1000

    refactor(nvim-lsp): make servers even more dynamic

--- nvim/.config/nvim/lua/daneharnett/lsp.lua
+++ nvim/.config/nvim/lua/daneharnett/lsp.lua
@@ -1,5 +1,156 @@
 local M = {}
 
+local server_configs = {
+    denols = function(capabilities)
+        local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
+        if not lspconfig_util_status_ok then
+            return
+        end
+
+        return {
+            capabilities = capabilities,
+            filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+            on_attach = M.make_on_attach(),
+            root_dir = lspconfig_util.root_pattern("deno.json", "deno.jsonc"),
+        }
+    end,
+    jsonls = function(capabilities)
+        return {
+            capabilities = capabilities,
+            filetypes = { "json", "jsonc" },
+            on_attach = M.make_on_attach(),
+            settings = {
+                json = {
+                    -- Schemas https://www.schemastore.org
+                    schemas = {
+                        {
+                            fileMatch = { "package.json" },
+                            url = "https://json.schemastore.org/package.json",
+                        },
+                        {
+                            fileMatch = { "manifest.json", "manifest.webmanifest" },
+                            url = "https://json.schemastore.org/web-manifest-combined.json",
+                        },
+                        {
+                            fileMatch = { "tsconfig*.json" },
+                            url = "https://json.schemastore.org/tsconfig.json",
+                        },
+                        {
+                            fileMatch = {
+                                ".prettierrc",
+                                ".prettierrc.json",
+                                "prettier.config.json",
+                            },
+                            url = "https://json.schemastore.org/prettierrc.json",
+                        },
+                        {
+                            fileMatch = { ".eslintrc", ".eslintrc.json" },
+                            url = "https://json.schemastore.org/eslintrc.json",
+                        },
+                        {
+                            fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
+                            url = "https://json.schemastore.org/babelrc.json",
+                        },
+                        {
+                            fileMatch = { "lerna.json" },
+                            url = "https://json.schemastore.org/lerna.json",
+                        },
+                        {
+                            fileMatch = { "now.json", "vercel.json" },
+                            url = "https://json.schemastore.org/now.json",
+                        },
+                        {
+                            fileMatch = {
+                                ".stylelintrc",
+                                ".stylelintrc.json",
+                                "stylelint.config.json",
+                            },
+                            url = "http://json.schemastore.org/stylelintrc.json",
+                        },
+                    },
+                },
+            },
+        }
+    end,
+    rust_analyzer = function(capabilities)
+        return {
+            capabilities = capabilities,
+            on_attach = M.make_on_attach({
+                on_after = function(_, bufnr)
+                    -- use rust_analyzer to format on save
+                    local group = vim.api.nvim_create_augroup("RustLspFormatting", { clear = false })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        buffer = bufnr,
+                        callback = function()
+                            vim.lsp.buf.format()
+                        end,
+                        group = group,
+                    })
+                end,
+            }),
+        }
+    end,
+    sumneko_lua = function(capabilities)
+        local runtime_path = vim.split(package.path, ";")
+        table.insert(runtime_path, "lua/?.lua")
+        table.insert(runtime_path, "lua/?/init.lua")
+
+        return {
+            capabilities = capabilities,
+            on_attach = M.make_on_attach(),
+            settings = {
+                Lua = {
+                    runtime = {
+                        version = "LuaJIT",
+                        path = runtime_path,
+                    },
+                    diagnostics = {
+                        globals = { "vim" },
+                    },
+                    workspace = {
+                        library = vim.api.nvim_get_runtime_file("", true),
+                    },
+                    telemetry = {
+                        enable = false,
+                    },
+                },
+            },
+        }
+    end,
+    tsserver = function(capabilities)
+        local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
+        if not lspconfig_util_status_ok then
+            return
+        end
+
+        return {
+            capabilities = capabilities,
+            filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
+            init_options = {
+                maxTsServerMemory = 12288,
+                preferences = {
+                    importModuleSpecifierPreference = "relative",
+                },
+            },
+            on_attach = M.make_on_attach({
+                on_after = function(client)
+                    -- Need to disable formatting for tsserver because we will use eslint or prettier instead
+                    client.server_capabilities.documentFormattingProvider = false
+                    client.server_capabilities.documentRangeFormattingProvider = false
+                end,
+            }),
+            -- configure tsserver for use in monorepos, spawn one process at the root.
+            -- check for tsconfig.json so we don't conflict with deno projects.
+            root_dir = function(filepath)
+                return (
+                    lspconfig_util.root_pattern(".git")(filepath)
+                    and lspconfig_util.root_pattern("tsconfig.json")(filepath)
+                )
+            end,
+        }
+    end,
+}
+
 function M.init()
     local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
     if not lspconfig_status_ok then
@@ -17,34 +168,33 @@ function M.init()
     if not cmp_nvim_lsp_status_ok then
         return
     end
-    local fidget_status_ok, fidget = pcall(require, "fidget")
-    if not fidget_status_ok then
-        return
-    end
 
     local client_capabilities = vim.lsp.protocol.make_client_capabilities()
     local capabilities =
         vim.tbl_deep_extend("force", client_capabilities, cmp_nvim_lsp.default_capabilities(client_capabilities))
 
-    local servers = {
-        denols = M.make_denols(capabilities),
-        jsonls = M.make_jsonls(capabilities),
-        rust_analyzer = M.make_rust_analyzer(capabilities),
-        sumneko_lua = M.make_sumneko_lua(capabilities),
-        tsserver = M.make_tsserver(capabilities),
-    }
+    local servers = {}
+    for server_name, make_server_config in pairs(server_configs) do
+        local server_config = make_server_config(capabilities)
+        if server_config then
+            servers[server_name] = server_config
+        end
+    end
 
     mason.setup()
     mason_lspconfig.setup({
         ensure_installed = vim.tbl_keys(servers),
     })
-
     mason_lspconfig.setup_handlers({
         function(server_name)
             lspconfig[server_name].setup(servers[server_name])
         end,
     })
 
+    local fidget_status_ok, fidget = pcall(require, "fidget")
+    if not fidget_status_ok then
+        return
+    end
     fidget.setup({})
 end
 
@@ -100,159 +250,6 @@ M.make_on_attach = function(opts)
             opts.on_after(client, bufnr)
         end
     end
-end
-
-M.make_denols = function(capabilities)
-    local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
-    if not lspconfig_util_status_ok then
-        return
-    end
-
-    return {
-        capabilities = capabilities,
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-        on_attach = M.make_on_attach(),
-        root_dir = lspconfig_util.root_pattern("deno.json", "deno.jsonc"),
-    }
-end
-
-M.make_jsonls = function(capabilities)
-    return {
-        capabilities = capabilities,
-        filetypes = { "json", "jsonc" },
-        on_attach = M.make_on_attach(),
-        settings = {
-            json = {
-                -- Schemas https://www.schemastore.org
-                schemas = {
-                    {
-                        fileMatch = { "package.json" },
-                        url = "https://json.schemastore.org/package.json",
-                    },
-                    {
-                        fileMatch = { "manifest.json", "manifest.webmanifest" },
-                        url = "https://json.schemastore.org/web-manifest-combined.json",
-                    },
-                    {
-                        fileMatch = { "tsconfig*.json" },
-                        url = "https://json.schemastore.org/tsconfig.json",
-                    },
-                    {
-                        fileMatch = {
-                            ".prettierrc",
-                            ".prettierrc.json",
-                            "prettier.config.json",
-                        },
-                        url = "https://json.schemastore.org/prettierrc.json",
-                    },
-                    {
-                        fileMatch = { ".eslintrc", ".eslintrc.json" },
-                        url = "https://json.schemastore.org/eslintrc.json",
-                    },
-                    {
-                        fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
-                        url = "https://json.schemastore.org/babelrc.json",
-                    },
-                    {
-                        fileMatch = { "lerna.json" },
-                        url = "https://json.schemastore.org/lerna.json",
-                    },
-                    {
-                        fileMatch = { "now.json", "vercel.json" },
-                        url = "https://json.schemastore.org/now.json",
-                    },
-                    {
-                        fileMatch = {
-                            ".stylelintrc",
-                            ".stylelintrc.json",
-                            "stylelint.config.json",
-                        },
-                        url = "http://json.schemastore.org/stylelintrc.json",
-                    },
-                },
-            },
-        },
-    }
-end
-
-M.make_rust_analyzer = function(capabilities)
-    return {
-        capabilities = capabilities,
-        on_attach = M.make_on_attach({
-            on_after = function(_, bufnr)
-                -- use rust_analyzer to format on save
-                local group = vim.api.nvim_create_augroup("RustLspFormatting", { clear = false })
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    buffer = bufnr,
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end,
-                    group = group,
-                })
-            end,
-        }),
-    }
-end
-
-M.make_sumneko_lua = function(capabilities)
-    local runtime_path = vim.split(package.path, ";")
-    table.insert(runtime_path, "lua/?.lua")
-    table.insert(runtime_path, "lua/?/init.lua")
-
-    return {
-        capabilities = capabilities,
-        on_attach = M.make_on_attach(),
-        settings = {
-            Lua = {
-                runtime = {
-                    version = "LuaJIT",
-                    path = runtime_path,
-                },
-                diagnostics = {
-                    globals = { "vim" },
-                },
-                workspace = {
-                    library = vim.api.nvim_get_runtime_file("", true),
-                },
-                telemetry = {
-                    enable = false,
-                },
-            },
-        },
-    }
-end
-
-M.make_tsserver = function(capabilities)
-    local lspconfig_util_status_ok, lspconfig_util = pcall(require, "lspconfig.util")
-    if not lspconfig_util_status_ok then
-        return
-    end
-
-    return {
-        capabilities = capabilities,
-        filetypes = { "typescript", "typescriptreact", "typescript.tsx" },
-        init_options = {
-            maxTsServerMemory = 12288,
-            preferences = {
-                importModuleSpecifierPreference = "relative",
-            },
-        },
-        on_attach = M.make_on_attach({
-            on_after = function(client)
-                -- Need to disable formatting for tsserver because we will use eslint or prettier instead
-                client.server_capabilities.documentFormattingProvider = false
-                client.server_capabilities.documentRangeFormattingProvider = false
-            end,
-        }),
-        -- configure tsserver for use in monorepos, spawn one process at the root.
-        -- check for tsconfig.json so we don't conflict with deno projects.
-        root_dir = function(filepath)
-            return (
-                lspconfig_util.root_pattern(".git")(filepath)
-                and lspconfig_util.root_pattern("tsconfig.json")(filepath)
-            )
-        end,
-    }
 end
 
 return M
