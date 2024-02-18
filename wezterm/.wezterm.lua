6c49f67508bbc65b70d3554492a9596aa25710e9
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2024-02-18 16:26:53 +1000

    feat(wezterm): remove title bar from window

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,25 +1,26 @@
 local wezterm = require("wezterm")
 return {
-	font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" }),
-	font_rules = {
-		{
-			italic = true,
-			font = wezterm.font_with_fallback({ "JetBrains Mono" }),
-		},
-	},
-	font_size = 16.0,
-	harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-	keys = {
-		{
-			key = "-",
-			mods = "CTRL",
-			action = wezterm.action.DisableDefaultAssignment,
-		},
-		{
-			key = "=",
-			mods = "CTRL",
-			action = wezterm.action.DisableDefaultAssignment,
-		},
-	},
-	use_fancy_tab_bar = false,
+  font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" }),
+  font_rules = {
+    {
+      italic = true,
+      font = wezterm.font_with_fallback({ "JetBrains Mono" }),
+    },
+  },
+  font_size = 16.0,
+  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+  keys = {
+    {
+      key = "-",
+      mods = "CTRL",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+    {
+      key = "=",
+      mods = "CTRL",
+      action = wezterm.action.DisableDefaultAssignment,
+    },
+  },
+  use_fancy_tab_bar = false,
+  window_decorations = "RESIZE",
 }
