84782e035fb74eadef6ac80d3dbad3049a8f0c2e
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2024-03-09 16:07:41 +1000

    feat(nvim/fzf-lua): install fzf-lua

--- nvim/.config/nvim/lua/daneharnett/plugins.lua
+++ nvim/.config/nvim/lua/daneharnett/plugins.lua
@@ -306,6 +306,24 @@ local plugins = {
             require("daneharnett.autosession").init()
         end,
     },
+    -- trialling fzf-lua because telescope can be slow for my large repos
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("fzf-lua").setup({})
+
+            vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function()
+                require("fzf-lua").complete_path()
+            end, { silent = true, desc = "Fuzzy complete path" })
+
+            vim.keymap.set({ "n", "v", "i" }, "<C-x><C-x>", function()
+                require("fzf-lua").complete_path({
+                    cmd = "fd --type=d --hidden --exclude=.git --exclude=node_modules",
+                })
+            end, { silent = true, desc = "Fuzzy complete directory path" })
+        end,
+    },
 }
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
