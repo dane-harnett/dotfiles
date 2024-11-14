61ecf59136e405d747f51aebbf27446c73e1da82
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2024-11-14 17:48:56 +1000

    add m4mbp nix config

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -94,6 +94,7 @@
   {
     darwinConfigurations = {
       "personal-i9mbp" = mkDarwinConfig "personal-i9mbp" "x86_64-darwin" "dane";
+      "personal-m4mbp" = mkDarwinConfig "personal-m4mbp" "aarch64-darwin" "dane";
       # "work-m1mbp" = mkDarwinConfig "work-m1mbp" "aarch64-darwin" "dharnett";
     };
   };
