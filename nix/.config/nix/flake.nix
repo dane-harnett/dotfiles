b1898e8e2990d7ca191955d9d742eba62de70b9e
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-02-13 09:31:02 +1000

    Update nix flake

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -27,8 +27,6 @@
       # Set Git commit hash for darwin-version.
       system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Auto upgrade nix package and the daemon service.
-      services.nix-daemon.enable = true;
       # nix.package = pkgs.nix;
 
       # Necessary for using flakes on this system.
