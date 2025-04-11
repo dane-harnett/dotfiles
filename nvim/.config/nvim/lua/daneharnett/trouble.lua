f19c252f70f0ef856f9fe38ab13d1de7dee19ed2
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-04-11 15:17:25 +1000

    Fix trouble in nvim

--- nvim/.config/nvim/lua/daneharnett/trouble.lua
+++ nvim/.config/nvim/lua/daneharnett/trouble.lua
@@ -6,7 +6,7 @@ end
 
 function M.attach_keymaps_to_buffer(bufnr)
     local utils = require("daneharnett.utils")
-    utils.buffer_keymap(bufnr, "n", "<leader>gr", "<cmd>TroubleToggle lsp_references<cr>")
+    utils.buffer_keymap(bufnr, "n", "<leader>gr", "<cmd>Trouble lsp_references<cr>")
 end
 
 return M
