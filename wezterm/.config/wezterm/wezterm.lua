560dcb9251e49627a4a1566188666a55a09b6d78
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-09-17 19:34:39 +1000

    Wezterm color scheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" })
 config.font_rules = {
   {
