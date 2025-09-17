3d166a305d099acec8621d0e02d064703949b013
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-09-17 19:29:39 +1000

    Wezterm config using config_builder

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,26 +1,29 @@
 local wezterm = require("wezterm")
-return {
-  font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" }),
-  font_rules = {
-    {
-      italic = true,
-      font = wezterm.font_with_fallback({ "JetBrains Mono" }),
-    },
+local config = wezterm.config_builder()
+
+config.font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font" })
+config.font_rules = {
+  {
+    italic = true,
+    font = wezterm.font_with_fallback({ "JetBrains Mono" }),
   },
-  font_size = 16.0,
-  harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  keys = {
-    {
-      key = "-",
-      mods = "CTRL",
-      action = wezterm.action.DisableDefaultAssignment,
-    },
-    {
-      key = "=",
-      mods = "CTRL",
-      action = wezterm.action.DisableDefaultAssignment,
-    },
-  },
-  use_fancy_tab_bar = false,
-  window_decorations = "RESIZE",
 }
+config.font_size = 16.0
+-- If you want to disable ligatures in most fonts, then you may want to use a setting like this:
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
+config.keys = {
+  {
+    key = "-",
+    mods = "CTRL",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+  {
+    key = "=",
+    mods = "CTRL",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+config.use_fancy_tab_bar = false
+config.window_decorations = "RESIZE"
+
+return config
