0ea9c49336c7c0daeb7d315bc3c5e42df1603f19
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-09-25 18:38:40 +1000

    nvim refactor telescope config

--- nvim/.config/nvim/lua/daneharnett/telescope.lua
+++ nvim/.config/nvim/lua/daneharnett/telescope.lua
@@ -1,24 +1,15 @@
 local M = {}
 
 function M.init()
-    M.attach_keymaps()
-
-    local buffers = { preview = true }
-    local git_files = {}
-    local grep_string = { preview = true }
-    local live_grep = {
-        additional_args = function()
-            return { "--hidden" }
-        end,
-    }
-    local oldfiles = {}
-
     local telescope = require("telescope")
-    local telescope_actions = require("telescope.actions")
+    local actions = require("telescope.actions")
+    local previewers = require("telescope.previewers")
+    local sorters = require("telescope.sorters")
+    local themes = require("telescope.themes")
 
     telescope.setup({
         defaults = {
-            file_sorter = require("telescope.sorters").get_fzy_sorter,
+            file_sorter = sorters.get_fzy_sorter,
             path_display = {
                 truncate = true,
             },
@@ -32,13 +23,13 @@ function M.init()
             },
             mappings = {
                 i = {
-                    ["<C-D>"] = telescope_actions.cycle_history_next,
-                    ["<C-U>"] = telescope_actions.cycle_history_prev,
+                    ["<C-D>"] = actions.cycle_history_next,
+                    ["<C-U>"] = actions.cycle_history_prev,
                 },
             },
-            file_previewer = require("telescope.previewers").vim_buffer_cat.new,
-            grep_previewer = require("telescope.previewers").vim_buffer_vimgrep.new,
-            qflist_previewer = require("telescope.previewers").vim_buffer_qflist.new,
+            file_previewer = previewers.vim_buffer_cat.new,
+            grep_previewer = previewers.vim_buffer_vimgrep.new,
+            qflist_previewer = previewers.vim_buffer_qflist.new,
         },
         extensions = {
             fzf = {
@@ -48,190 +39,40 @@ function M.init()
                 case_mode = "smart_case",
             },
             ["ui-select"] = {
-                require("telescope.themes").get_dropdown(),
+                themes.get_dropdown(),
             },
         },
         pickers = {
-            buffers = buffers,
+            buffers = { preview = true },
             find_files = M.get_find_files(),
-            git_files = git_files,
-            grep_string = grep_string,
-            live_grep = live_grep,
-            oldfiles = oldfiles,
+            git_files = {},
+            grep_string = { preview = true },
+            live_grep = {
+                additional_args = function()
+                    return { "--hidden" }
+                end,
+            },
+            oldfiles = {},
         },
     })
 
-    pcall(require("telescope").load_extension, "fzf")
-    pcall(require("telescope").load_extension, "harpoon")
-    pcall(require("telescope").load_extension, "ui-select")
+    M.load_extensions()
+    M.create_user_commands()
+    M.attach_keymaps()
+end
 
+M.load_extensions = function()
+    local telescope = require("telescope")
+    pcall(telescope.load_extension, "fzf")
+    pcall(telescope.load_extension, "harpoon")
+    pcall(telescope.load_extension, "ui-select")
+end
+
+M.create_user_commands = function()
     vim.api.nvim_create_user_command("TelescopeFindFilesIncludingHidden", M.find_files_including_hidden, {})
-
-    local conf = require("telescope.config").values
-    local finders = require("telescope.finders")
-    local make_entry = require("telescope.make_entry")
-    local pickers = require("telescope.pickers")
-
-    local TMP_LGWG_PROMPT_FILENAME = "/tmp/nvim_daneharnett_live_grep_with_globs_prompt"
-
-    local function read_saved_lgwg_prompt()
-        local tmp_file = nil
-
-        local read_file = function()
-            tmp_file = io.input(TMP_LGWG_PROMPT_FILENAME)
-        end
-
-        if pcall(read_file) then
-            return tmp_file:read()
-        else
-            return ""
-        end
-    end
-
-    local function save_lgwg_prompt(prompt)
-        local tmp_file = io.output(TMP_LGWG_PROMPT_FILENAME)
-        if tmp_file then
-            tmp_file:write(prompt)
-            tmp_file:flush()
-        end
-    end
-
-    vim.api.nvim_create_user_command("TelescopeLiveGrepWithGlobs", function()
-        local live_grep_with_globs = finders.new_async_job({
-            command_generator = function(prompt)
-                if not prompt or prompt == "" then
-                    return nil
-                end
-
-                local prompt_split = vim.split(prompt, "  ")
-
-                if not prompt_split[1] or not prompt_split[2] or prompt_split[2] == "" then
-                    return nil
-                end
-
-                local args = { "rg" }
-
-                -- apply the globs
-                local pattern = prompt_split[1]
-                local split_pattern = vim.split(pattern, ",")
-
-                for _, patt in pairs(split_pattern) do
-                    patt = string.gsub(patt, "^%s*(.-)%s*$", "%1")
-                    if patt ~= "" then
-                        table.insert(args, string.format("--glob=%s", patt))
-                    end
-                end
-
-                save_lgwg_prompt(prompt)
-
-                -- apply the search query
-                local query = prompt_split[2]
-
-                -- a prompt is "regex" if it begins and ends with `/`
-                local is_regex_query = string.sub(query, 1, 1) == "/"
-                    and string.sub(query, string.len(query), string.len(query)) == "/"
-
-                if is_regex_query then
-                    local query_without_slashes = string.sub(query, 2, string.len(query) - 1)
-                    table.insert(args, string.format("--regexp=%s", query_without_slashes))
-                else
-                    table.insert(args, "--fixed-strings")
-                end
-
-                local command = vim.tbl_flatten({
-                    args,
-                    {
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--smart-case",
-                        "--hidden",
-                    },
-                })
-
-                if not is_regex_query then
-                    table.insert(command, query)
-                end
-
-                return command
-            end,
-            entry_maker = make_entry.gen_from_vimgrep(),
-        })
-
-        pickers
-            .new({
-                file_ignore_patterns = { ".git/" },
-            }, {
-                debounce = 100,
-                default_text = read_saved_lgwg_prompt(),
-                prompt_title = "Live grep with globs",
-                finder = live_grep_with_globs,
-                previewer = conf.grep_previewer({}),
-                sorter = require("telescope.sorters").highlighter_only(),
-            })
-            :find()
-    end, {})
-
-    vim.api.nvim_create_user_command("TelescopeCustomLiveGrep", function()
-        local custom_live_grep = finders.new_async_job({
-            command_generator = function(prompt)
-                if not prompt or prompt == "" then
-                    return nil
-                end
-
-                local query = prompt
-
-                local args = { "rg" }
-
-                -- is "regex" if it begins and ends with `/`
-                local is_regex_query = string.sub(query, 1, 1) == "/"
-                    and string.sub(query, string.len(query), string.len(query)) == "/"
-
-                if is_regex_query then
-                    local query_without_slashes = string.sub(query, 2, string.len(query) - 1)
-                    table.insert(args, string.format("--regexp=%s", query_without_slashes))
-                else
-                    table.insert(args, "--fixed-strings")
-                end
-
-                local command = vim.tbl_flatten({
-                    args,
-                    {
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--smart-case",
-                        "--hidden",
-                    },
-                })
-
-                if not is_regex_query then
-                    table.insert(command, query)
-                end
-
-                return command
-            end,
-            entry_maker = make_entry.gen_from_vimgrep(),
-        })
-
-        pickers
-            .new({
-                file_ignore_patterns = { ".git/" },
-            }, {
-                debounce = 100,
-                prompt_title = "Live grep",
-                finder = custom_live_grep,
-                previewer = conf.grep_previewer({}),
-                sorter = require("telescope.sorters").highlighter_only(),
-            })
-            :find()
-    end, {})
-
-    M.insert_path_to_directory()
+    vim.api.nvim_create_user_command("TelescopeCustomLiveGrep", M.live_grep_with_globs, {})
+    vim.api.nvim_create_user_command("TelescopeLiveGrepWithGlobs", M.live_grep_with_globs, {})
+    vim.api.nvim_create_user_command("TelescopeInsertPathToDirectory", M.insert_path_to_directory, {})
 end
 
 M.attach_keymaps = function()
@@ -249,32 +90,172 @@ M.attach_keymaps = function()
     utils.keymap("n", "<LEADER>fdp", "<CMD>TelescopeInsertPathToDirectory<CR>")
 end
 
+M.live_grep_with_globs = function()
+    local conf = require("telescope.config").values
+    local finders = require("telescope.finders")
+    local make_entry = require("telescope.make_entry")
+    local pickers = require("telescope.pickers")
+
+    local live_grep_with_globs = finders.new_async_job({
+        command_generator = function(prompt)
+            if not prompt or prompt == "" then
+                return nil
+            end
+
+            local prompt_split = vim.split(prompt, "  ")
+
+            if not prompt_split[1] or not prompt_split[2] or prompt_split[2] == "" then
+                return nil
+            end
+
+            local args = { "rg" }
+
+            -- apply the globs
+            local pattern = prompt_split[1]
+            local split_pattern = vim.split(pattern, ",")
+
+            for _, patt in pairs(split_pattern) do
+                patt = string.gsub(patt, "^%s*(.-)%s*$", "%1")
+                if patt ~= "" then
+                    table.insert(args, string.format("--glob=%s", patt))
+                end
+            end
+
+            -- apply the search query
+            local query = prompt_split[2]
+
+            -- a prompt is "regex" if it begins and ends with `/`
+            local is_regex_query = string.sub(query, 1, 1) == "/"
+                and string.sub(query, string.len(query), string.len(query)) == "/"
+
+            if is_regex_query then
+                local query_without_slashes = string.sub(query, 2, string.len(query) - 1)
+                table.insert(args, string.format("--regexp=%s", query_without_slashes))
+            else
+                table.insert(args, "--fixed-strings")
+            end
+
+            local command = vim.tbl_flatten({
+                args,
+                {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                },
+            })
+
+            if not is_regex_query then
+                table.insert(command, query)
+            end
+
+            return command
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(),
+    })
+
+    pickers
+        .new({
+            file_ignore_patterns = { ".git/" },
+        }, {
+            debounce = 100,
+            prompt_title = "Live grep with globs",
+            finder = live_grep_with_globs,
+            previewer = conf.grep_previewer({}),
+            sorter = require("telescope.sorters").highlighter_only(),
+        })
+        :find()
+end
+
+M.custom_live_grep = function()
+    local conf = require("telescope.config").values
+    local finders = require("telescope.finders")
+    local make_entry = require("telescope.make_entry")
+    local pickers = require("telescope.pickers")
+
+    local custom_live_grep = finders.new_async_job({
+        command_generator = function(prompt)
+            if not prompt or prompt == "" then
+                return nil
+            end
+
+            local query = prompt
+
+            local args = { "rg" }
+
+            -- is "regex" if it begins and ends with `/`
+            local is_regex_query = string.sub(query, 1, 1) == "/"
+                and string.sub(query, string.len(query), string.len(query)) == "/"
+
+            if is_regex_query then
+                local query_without_slashes = string.sub(query, 2, string.len(query) - 1)
+                table.insert(args, string.format("--regexp=%s", query_without_slashes))
+            else
+                table.insert(args, "--fixed-strings")
+            end
+
+            local command = vim.tbl_flatten({
+                args,
+                {
+                    "--color=never",
+                    "--no-heading",
+                    "--with-filename",
+                    "--line-number",
+                    "--column",
+                    "--smart-case",
+                    "--hidden",
+                },
+            })
+
+            if not is_regex_query then
+                table.insert(command, query)
+            end
+
+            return command
+        end,
+        entry_maker = make_entry.gen_from_vimgrep(),
+    })
+
+    pickers
+        .new({
+            file_ignore_patterns = { ".git/" },
+        }, {
+            debounce = 100,
+            prompt_title = "Live grep",
+            finder = custom_live_grep,
+            previewer = conf.grep_previewer({}),
+            sorter = require("telescope.sorters").highlighter_only(),
+        })
+        :find()
+end
+
 M.insert_path_to_directory = function()
+    local opts = {
+        attach_mappings = M.insert_selected_entry,
+        find_command = {
+            "fd",
+            "--type=d",
+            "--hidden",
+            "--exclude=.git",
+            "--exclude=node_modules",
+        },
+    }
+    require("telescope.builtin").find_files(opts)
+end
+
+M.insert_selected_entry = function(prompt_bufnr)
     local actions = require("telescope.actions")
     local action_state = require("telescope.actions.state")
 
-    local function run_selection(prompt_bufnr)
-        actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            vim.api.nvim_put({ selection[1] }, "c", true, true)
-        end)
-        return true
-    end
-
-    vim.api.nvim_create_user_command("TelescopeInsertPathToDirectory", function()
-        local opts = {
-            attach_mappings = run_selection,
-            find_command = {
-                "fd",
-                "--type=d",
-                "--hidden",
-                "--exclude=.git",
-                "--exclude=node_modules",
-            },
-        }
-        require("telescope.builtin").find_files(opts)
-    end, {})
+    actions.select_default:replace(function()
+        actions.close(prompt_bufnr)
+        local selection = action_state.get_selected_entry()
+        vim.api.nvim_put({ selection[1] }, "c", true, true)
+    end)
+    return true
 end
 
 M.get_base_find_command = function()
