9290d2de19b06def2b59924b8c028f0c7a20ca0a
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2025-10-13 20:27:58 +1000

    format nix flake.nix

--- nix/.config/nix/flake.nix
+++ nix/.config/nix/flake.nix
@@ -15,89 +15,96 @@
     };
   };
 
-  outputs = inputs@{
-    self,
-    nix-darwin,
-    nixpkgs,
-    nix-homebrew,
-    home-manager
-  }:
-  let
-    configuration = { pkgs, ... }: {
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+  outputs =
+    inputs@{
+      self,
+      nix-darwin,
+      nixpkgs,
+      nix-homebrew,
+      home-manager,
+    }:
+    let
+      configuration =
+        { pkgs, ... }:
+        {
+          # Set Git commit hash for darwin-version.
+          system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # nix.package = pkgs.nix;
+          # nix.package = pkgs.nix;
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+          # Necessary for using flakes on this system.
+          nix.settings.experimental-features = "nix-command flakes";
 
-      nixpkgs.config.allowUnfree = true;
+          nixpkgs.config.allowUnfree = true;
 
-      system.stateVersion = 5;
-    };
-    mkDarwinConfig = host: system: username: let
-      pkgs = import inputs.nixpkgs {inherit system;};
-      madeConfig = {
-        nixpkgs.hostPlatform = "${system}";
-      };
+          system.stateVersion = 5;
+        };
+      mkDarwinConfig =
+        host: system: username:
+        let
+          pkgs = import inputs.nixpkgs { inherit system; };
+          madeConfig = {
+            nixpkgs.hostPlatform = "${system}";
+          };
+        in
+        nix-darwin.lib.darwinSystem {
+          modules = [
+            madeConfig
+            configuration
+
+            ./hosts/${host}.nix
+
+            ./modules/aerospace
+            ./modules/antidote
+            ./modules/bitwarden
+            ./modules/borders
+            ./modules/carapace
+            ./modules/cargo
+            ./modules/discord
+            ./modules/eza
+            ./modules/fd
+            ./modules/fnm
+            ./modules/font-jetbrains-mono-nerd-font
+            ./modules/fx-cast-bridge
+            ./modules/fzf
+            ./modules/gnu-sed
+            ./modules/neovim
+            ./modules/nix-tools
+            ./modules/obs
+            ./modules/oh-my-posh
+            ./modules/raycast
+            ./modules/ripgrep
+            ./modules/sketchybar
+            ./modules/slack
+            ./modules/stow
+            ./modules/util-linux
+            ./modules/vlc
+            ./modules/vscode
+            ./modules/wezterm
+            ./modules/wget
+            ./modules/yt-dlp
+            ./modules/zoxide
+
+            nix-homebrew.darwinModules.nix-homebrew
+            {
+              nix-homebrew = {
+                enable = true;
+                user = "${username}";
+              };
+            }
+
+            home-manager.darwinModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.${username} = import ./home/${username}/${host}.nix;
+            }
+          ];
+        };
     in
-      nix-darwin.lib.darwinSystem {
-        modules = [
-          madeConfig
-          configuration
-
-          ./hosts/${host}.nix
-
-          ./modules/aerospace
-          ./modules/antidote
-          ./modules/bitwarden
-          ./modules/borders
-          ./modules/carapace
-          ./modules/cargo
-          ./modules/discord
-          ./modules/eza
-          ./modules/fd
-          ./modules/fnm
-          ./modules/font-jetbrains-mono-nerd-font
-          ./modules/fx-cast-bridge
-          ./modules/fzf
-          ./modules/gnu-sed
-          ./modules/neovim
-          ./modules/nix-tools
-          ./modules/obs
-          ./modules/oh-my-posh
-          ./modules/raycast
-          ./modules/ripgrep
-          ./modules/sketchybar
-          ./modules/slack
-          ./modules/stow
-          ./modules/util-linux
-          ./modules/vlc
-          ./modules/vscode
-          ./modules/wezterm
-          ./modules/wget
-          ./modules/yt-dlp
-          ./modules/zoxide
-
-          nix-homebrew.darwinModules.nix-homebrew {
-            nix-homebrew = {
-              enable = true;
-              user = "${username}";
-            };
-          }
-
-          home-manager.darwinModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.${username} = import ./home/${username}/${host}.nix;
-          }
-        ];
+    {
+      darwinConfigurations = {
+        "personal-m4mbp" = mkDarwinConfig "personal-m4mbp" "aarch64-darwin" "dane";
       };
-  in
-  {
-    darwinConfigurations = {
-      "personal-m4mbp" = mkDarwinConfig "personal-m4mbp" "aarch64-darwin" "dane";
     };
-  };
 }
