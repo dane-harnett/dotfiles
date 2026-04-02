336207733a91ae24d89f14daf5a0722926af754c
Dane Harnett <2585257+dane-harnett@users.noreply.github.com>
2026-04-03 00:01:21 +1000

    feat(nvim): migrate from lazy.nvim to 0.12 vim.pack

--- nvim/.config/nvim/lua/daneharnett/plugins.lua
+++ nvim/.config/nvim/lua/daneharnett/plugins.lua
@@ -1,425 +1,153 @@
-local plugins = {
-    -- theme
+vim.pack.add({
+    "https://github.com/catppuccin/nvim",
+    "https://github.com/rcarriga/nvim-notify",
+    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "https://github.com/nvim-treesitter/nvim-treesitter-context",
+    "https://github.com/nvim-tree/nvim-web-devicons",
+    "https://github.com/akinsho/bufferline.nvim",
+    "https://github.com/nvim-lualine/lualine.nvim",
+    "https://github.com/folke/lazydev.nvim",
+    "https://github.com/mason-org/mason.nvim",
+    "https://github.com/mason-org/mason-lspconfig.nvim",
+    "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
     {
-        "catppuccin/nvim",
-        cond = not vim.g.vscode,
-        config = function()
-            require("daneharnett.catppuccin").init()
-        end,
-        lazy = false,
-        name = "catppuccin",
-        -- colorschemes need high priority
-        priority = 1000,
+        src = "https://github.com/j-hui/fidget.nvim",
+        version = "legacy",
     },
-    -- enhanced vim.notify ui
-    { "rcarriga/nvim-notify" },
-    -- treesitter
+    "https://github.com/b0o/schemastore.nvim",
     {
-        "nvim-treesitter/nvim-treesitter",
-        lazy = false,
-        build = ":TSUpdate",
-        branch = "main",
-        config = function()
-            require("daneharnett.treesitter").init()
-        end,
+        src = "https://github.com/saghen/blink.cmp",
+        version = "v1.10.1",
+    },
+    "https://github.com/neovim/nvim-lspconfig",
+    "https://github.com/rshkarin/mason-nvim-lint",
+    "https://github.com/nvim-tree/nvim-tree.lua",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/ThePrimeagen/harpoon",
+    "https://github.com/folke/trouble.nvim",
+    "https://github.com/editorconfig/editorconfig-vim",
+    "https://github.com/psliwka/vim-smoothie",
+    "https://github.com/numToStr/Comment.nvim",
+    "https://github.com/windwp/nvim-autopairs",
+    "https://github.com/akinsho/toggleterm.nvim",
+    "https://github.com/vim-test/vim-test",
+    "https://github.com/norcalli/nvim-colorizer.lua",
+    "https://github.com/lewis6991/gitsigns.nvim",
+    "https://github.com/tpope/vim-fugitive",
+    "https://github.com/APZelos/blamer.nvim",
+    "https://github.com/nvim-pack/nvim-spectre",
+    "https://github.com/folke/todo-comments.nvim",
+    "https://github.com/MunifTanjim/nui.nvim",
+    "https://github.com/OlegGulevskyy/better-ts-errors.nvim",
+    "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/sindrets/diffview.nvim",
+    "https://github.com/nvim-lua/popup.nvim",
+    "https://github.com/nvim-telescope/telescope-fzf-native.nvim",
+    "https://github.com/nvim-telescope/telescope-ui-select.nvim",
+    "https://github.com/nvim-telescope/telescope.nvim",
+    "https://github.com/stevearc/conform.nvim",
+    "https://github.com/mfussenegger/nvim-lint",
+    "https://github.com/nvimdev/lspsaga.nvim",
+    "https://github.com/rafamadriz/friendly-snippets",
+    {
+        src = "https://github.com/L3MON4D3/LuaSnip",
+        version = "v2.4.1",
     },
     {
-        "nvim-treesitter/nvim-treesitter-context",
+        src = "https://github.com/saghen/blink.cmp",
+        version = "v1.10.1",
     },
-    -- nvim-biscuits
-    -- {
-    --     "code-biscuits/nvim-biscuits",
-    --     cond = not vim.g.vscode,
-    --     config = function()
-    --         require("daneharnett.biscuits").init()
-    --     end,
-    -- },
-    -- buffer line (top of buffer)
-    {
-        "akinsho/bufferline.nvim",
-        config = function()
-            require("daneharnett.bufferline").init()
-        end,
-        dependencies = {
-            {
-                "nvim-tree/nvim-web-devicons",
-            },
-        },
-    },
-    -- status line (bottom of buffer)
-    {
-        "nvim-lualine/lualine.nvim",
-        cond = not vim.g.vscode,
-        config = function()
-            require("daneharnett.lualine").init()
-        end,
-        dependencies = {
-            {
-                "nvim-tree/nvim-web-devicons",
-            },
-        },
-    },
-    -- lsp
-    {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
+})
+
+require("daneharnett.catppuccin").init()
+require("daneharnett.treesitter").init()
+require("daneharnett.bufferline").init()
+require("daneharnett.lualine").init()
+require("daneharnett.lsp").init()
+require("daneharnett.nvim-tree").init()
+require("daneharnett.trouble").init()
+require("daneharnett.comment").init()
+require("daneharnett.autopairs").init()
+require("daneharnett.toggleterm").init()
+require("daneharnett.tests").init()
+require("daneharnett.colorizer").init()
+require("daneharnett.gitsigns").init()
+require("daneharnett.blamer").init()
+require("daneharnett.spectre").init()
+require("daneharnett.better-ts-errors").init()
+require("daneharnett.telescope").init()
+
+local conform_group = vim.api.nvim_create_augroup("ConformLoader", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    group = conform_group,
+    once = true,
+    callback = function()
+        require("daneharnett.formatting").init()
+    end,
+})
+local nvimlint_group = vim.api.nvim_create_augroup("NvimLintLoader", { clear = true })
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+    group = nvimlint_group,
+    once = true,
+    callback = function()
+        require("daneharnett.linting").init()
+    end,
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "Load Lspsaga and dependencies when LSP attaches",
+    once = true,
+    callback = function()
+        require("daneharnett.lspsaga").init()
+    end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "lua",
+    once = true,
+    callback = function()
+        require("lazydev").setup({
             library = {
                 { path = "${3rd}/luv/library", words = { "vim%.uv" } },
             },
+        })
+    end,
+})
+
+-- TODO: put this in a fzf.init()
+require("fzf-lua").setup({})
+
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function()
+    require("fzf-lua").complete_path()
+end, { silent = true, desc = "Fuzzy complete path" })
+
+vim.keymap.set({ "n", "v", "i" }, "<C-x><C-x>", function()
+    require("fzf-lua").complete_path({
+        cmd = "fd --type=d --hidden --exclude=.git --exclude=node_modules",
+    })
+end, { silent = true, desc = "Fuzzy complete directory path" })
+
+
+-- TODO: put this in a blink-cmp.init()
+require("luasnip.loaders.from_vscode").lazy_load()
+
+require("blink.cmp").setup({
+    keymap = {
+        preset = "default",
+    },
+    appearance = {
+        nerd_font_variant = "mono",
+    },
+    completion = {
+        documentation = { auto_show = false, auto_show_delay_ms = 500 },
+    },
+    sources = {
+        default = { "lsp", "path", "snippets", "lazydev" },
+        providers = {
+            lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
         },
     },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require("daneharnett.lsp").init()
-        end,
-        dependencies = {
-            {
-                "williamboman/mason.nvim",
-            },
-            {
-                "williamboman/mason-lspconfig.nvim",
-            },
-            { "WhoIsSethDaniel/mason-tool-installer.nvim" },
-            {
-                "j-hui/fidget.nvim",
-                tag = "legacy",
-            },
-            {
-                "b0o/schemastore.nvim",
-            },
-            {
-                "saghen/blink.cmp",
-            },
-        },
-    },
-    { -- Autocompletion
-        "saghen/blink.cmp",
-        event = "VimEnter",
-        version = "1.*",
-        dependencies = {
-            -- Snippet Engine
-            {
-                "L3MON4D3/LuaSnip",
-                version = "2.*",
-                build = (function()
-                    -- Build Step is needed for regex support in snippets.
-                    -- This step is not supported in many windows environments.
-                    -- Remove the below condition to re-enable on windows.
-                    if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-                        return
-                    end
-                    return "make install_jsregexp"
-                end)(),
-                dependencies = {
-                    -- `friendly-snippets` contains a variety of premade snippets.
-                    --    See the README about individual language/framework/plugin snippets:
-                    --    https://github.com/rafamadriz/friendly-snippets
-                    -- {
-                    --   'rafamadriz/friendly-snippets',
-                    --   config = function()
-                    --     require('luasnip.loaders.from_vscode').lazy_load()
-                    --   end,
-                    -- },
-                },
-                opts = {},
-            },
-            "folke/lazydev.nvim",
-        },
-        --- @module 'blink.cmp'
-        --- @type blink.cmp.Config
-        opts = {
-            keymap = {
-                -- 'default' (recommended) for mappings similar to built-in completions
-                --   <c-y> to accept ([y]es) the completion.
-                --    This will auto-import if your LSP supports it.
-                --    This will expand snippets if the LSP sent a snippet.
-                -- 'super-tab' for tab to accept
-                -- 'enter' for enter to accept
-                -- 'none' for no mappings
-                --
-                -- For an understanding of why the 'default' preset is recommended,
-                -- you will need to read `:help ins-completion`
-                --
-                -- No, but seriously. Please read `:help ins-completion`, it is really good!
-                --
-                -- All presets have the following mappings:
-                -- <tab>/<s-tab>: move to right/left of your snippet expansion
-                -- <c-space>: Open menu or open docs if already open
-                -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-                -- <c-e>: Hide menu
-                -- <c-k>: Toggle signature help
-                --
-                -- See :h blink-cmp-config-keymap for defining your own keymap
-                preset = "default",
-
-                -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-                --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-            },
-
-            appearance = {
-                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = "mono",
-            },
-
-            completion = {
-                -- By default, you may press `<c-space>` to show the documentation.
-                -- Optionally, set `auto_show = true` to show the documentation after a delay.
-                documentation = { auto_show = false, auto_show_delay_ms = 500 },
-            },
-
-            sources = {
-                default = { "lsp", "path", "snippets", "lazydev" },
-                providers = {
-                    lazydev = { module = "lazydev.integrations.blink", score_offset = 100 },
-                },
-            },
-
-            snippets = { preset = "luasnip" },
-
-            -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-            -- which automatically downloads a prebuilt binary when enabled.
-            --
-            -- By default, we use the Lua implementation instead, but you may enable
-            -- the rust implementation via `'prefer_rust_with_warning'`
-            --
-            -- See :h blink-cmp-config-fuzzy for more information
-            fuzzy = { implementation = "prefer_rust" },
-
-            -- Shows a signature help window while you type arguments for a function
-            signature = { enabled = true },
-        },
-    },
-    {
-        "stevearc/conform.nvim",
-        config = function()
-            require("daneharnett.formatting").init()
-        end,
-        event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-    },
-    {
-        "mfussenegger/nvim-lint",
-        config = function()
-            require("daneharnett.linting").init()
-        end,
-        event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
-    },
-    {
-        "rshkarin/mason-nvim-lint",
-    },
-    -- Disabling via commenting this for now as it doesn't quite meet my needs,
-    -- more tinkering required.
-    -- {
-    --     "creativenull/diagnosticls-configs-nvim",
-    --     dependencies = {
-    --         {
-    --             "neovim/nvim-lspconfig",
-    --         },
-    --     },
-    --     config = function()
-    --         require("daneharnett.diagnosticls").init()
-    --     end,
-    -- },
-    -- {
-    --     "creativenull/efmls-configs-nvim",
-    --     dependencies = {
-    --         {
-    --             "neovim/nvim-lspconfig",
-    --         },
-    --     },
-    --     config = function()
-    --         require("daneharnett.efmls").init()
-    --     end,
-    -- },
-    {
-        "nvimdev/lspsaga.nvim",
-        config = function()
-            require("daneharnett.lspsaga").init()
-        end,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-        event = "LspAttach",
-    },
-    -- file-tree sidebar explorer
-    {
-        "nvim-tree/nvim-tree.lua",
-        config = function()
-            require("daneharnett.nvim-tree").init()
-        end,
-    },
-    -- harpoon
-    {
-        "ThePrimeagen/harpoon",
-        dependencies = {
-            {
-                "nvim-lua/plenary.nvim",
-            },
-        },
-    },
-    -- telescope
-    {
-        "nvim-telescope/telescope.nvim",
-        config = function()
-            require("daneharnett.telescope").init()
-        end,
-        dependencies = {
-            {
-                "nvim-lua/popup.nvim",
-            },
-            {
-                "nvim-lua/plenary.nvim",
-            },
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-            },
-            { "nvim-telescope/telescope-ui-select.nvim" },
-        },
-        event = "VeryLazy",
-    },
-    -- trouble
-    {
-        "folke/trouble.nvim",
-        config = function()
-            require("daneharnett.trouble").init()
-        end,
-        dependencies = {
-            {
-                "nvim-tree/nvim-web-devicons",
-            },
-        },
-    },
-    -- editorconfig
-    {
-        "editorconfig/editorconfig-vim",
-    },
-    -- vim-smoothie for smooth scrolling
-    {
-        "psliwka/vim-smoothie",
-    },
-    -- comments
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("daneharnett.comment").init()
-        end,
-    },
-    -- autopairs
-    {
-        "windwp/nvim-autopairs",
-        config = function()
-            require("daneharnett.autopairs").init()
-        end,
-    },
-    -- terminal
-    {
-        "akinsho/toggleterm.nvim",
-        config = function()
-            require("daneharnett.toggleterm").init()
-        end,
-    },
-    -- run tests
-    {
-        "vim-test/vim-test",
-        config = function()
-            require("daneharnett.tests").init()
-        end,
-    },
-    -- show colors visually
-    {
-        "norcalli/nvim-colorizer.lua",
-        config = function()
-            require("daneharnett.colorizer").init()
-        end,
-    },
-    -- git
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require("daneharnett.gitsigns").init()
-        end,
-    },
-    {
-        "tpope/vim-fugitive",
-    },
-    {
-        "APZelos/blamer.nvim",
-        config = function()
-            require("daneharnett.blamer").init()
-        end,
-    },
-    {
-        "nvim-pack/nvim-spectre",
-        config = function()
-            require("daneharnett.spectre").init()
-        end,
-        dependencies = {
-            {
-                "nvim-tree/nvim-web-devicons",
-                "nvim-lua/plenary.nvim",
-            },
-        },
-    },
-    -- NOTE: this plugin highlights these types of comments.
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {},
-    },
-    {
-        "OlegGulevskyy/better-ts-errors.nvim",
-        dependencies = { "MunifTanjim/nui.nvim" },
-        config = function()
-            require("daneharnett.better-ts-errors").init()
-        end,
-    },
-    -- {
-    --     "rmagatti/auto-session",
-    --     config = function()
-    --         require("daneharnett.autosession").init()
-    --     end,
-    -- },
-    -- trialling fzf-lua because telescope can be slow for my large repos
-    {
-        "ibhagwan/fzf-lua",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-        config = function()
-            require("fzf-lua").setup({})
-
-            vim.keymap.set({ "n", "v", "i" }, "<C-x><C-f>", function()
-                require("fzf-lua").complete_path()
-            end, { silent = true, desc = "Fuzzy complete path" })
-
-            vim.keymap.set({ "n", "v", "i" }, "<C-x><C-x>", function()
-                require("fzf-lua").complete_path({
-                    cmd = "fd --type=d --hidden --exclude=.git --exclude=node_modules",
-                })
-            end, { silent = true, desc = "Fuzzy complete directory path" })
-        end,
-    },
-    {
-        "sindrets/diffview.nvim",
-    },
-}
-
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-    if vim.v.shell_error ~= 0 then
-        vim.api.nvim_echo({
-            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-            { out, "WarningMsg" },
-            { "\nPress any key to exit..." },
-        }, true, {})
-        vim.fn.getchar()
-        os.exit(1)
-    end
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(plugins)
+    snippets = { preset = "luasnip" },
+    fuzzy = { implementation = "prefer_rust" },
+    signature = { enabled = true },
+})
